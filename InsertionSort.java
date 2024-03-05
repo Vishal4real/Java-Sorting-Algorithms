8af81363d4f1d046688b5e993730123b834cd60b
Vishal <leo3369706@gmail.com>
2024-03-05 13:30:51 +0530

    Insertion Sort

---
+++ InsertionSort.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+public class InsertionSort {
+    public static void main(String[] args) {
+        int arr[] = { 5, 3, 4, 1, 2 };
+        sortbyinsert(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    public static void sortbyinsert(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j > 0; j--) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+    public static void swap(int arr[], int first, int second) {
+        int temp = first;
+        arr[first] = arr[second];
+        arr[second] = temp;
+    }
+}
